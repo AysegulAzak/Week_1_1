94932637dc8b90e1fe1a431b383bdab81b0f7bcf
Ayşegül Azak <2110206031@ogrenci.karabuk.edu.tr>
2024-09-25 19:11:13 +0300

    Add project files.

---
+++ Week_1_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1_1
+{
+    class Bread
+    {
+        public int counter;
+    }
+
+    static class Butter
+    {
+        public static int counter;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bread a = new Bread();
+            a.counter = 5;
+            Console.WriteLine("Bread: " + a.counter);
+
+            Butter.counter = 7;
+
+            Console.WriteLine("Butter: " + Butter.counter);
+        }
+    }
+}
